ab9ea00e6b0f3618d05d6f0ea338c93464439d86
Martins Obayomi <obayomimartins96@gmail.com>
2020-08-26 13:34:00 +0100

    Sequelize init

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,19 +1,23 @@
-import 'dotenv/config';
-
-export const development = {
-  use_env_variable: 'DATABASE_URL',
-  logging: false,
-  dialect: 'postgres',
-};
-
-export const test = {
-  use_env_variable: 'TEST_DATABASE_URL',
-  logging: false,
-  dialect: 'postgres',
-};
-
-export const production = {
-  use_env_variable: 'DATABASE_URL',
-  logging: false,
-  dialect: 'postgres',
-};
+{
+  "development": {
+    "username": "root",
+    "password": null,
+    "database": "database_development",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
